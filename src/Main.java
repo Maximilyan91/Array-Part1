e176fed8964729b0df19fad0c6bec791d7b8ecba
Максим <gmax7911@gmail.com>
2022-12-13 19:06:24 +0300

    Задача 3 выполнена

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,40 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-       //Задача 1.
+        //Задача 3.
 
-        int[] massive1 = new int[12];
+        int[] massive1 = new int[3];
         massive1[0] = 1;
         massive1[1] = 2;
         massive1[2] = 3;
+        for (int i = massive1.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                System.out.print(massive1[i] + ", ");
+            } else {
+                System.out.println(massive1[i]);
+            }
+        }
 
-        System.out.print(Arrays.toString(massive1)); // Задача 2.
-        System.out.println();
 
         double[] massive2 = {1.57, 7.654, 9.986,};
-
-        System.out.print(Arrays.toString(massive2)); // Задача 2.
-        System.out.println();
-
-        String[] massive3 = {"one", "two", "three"};
-
-        System.out.print(Arrays.toString(massive3)); // Задача 2.
-
-        //Задача 3.
+        for (int a = massive2.length - 1; a >= 0; a--) {
+            if (a > 0) {
+                System.out.print(massive2[a] + ", ");
+            } else {
+                System.out.println(massive2[a]);
+                }
+            }
 
 
+            String[] massive3 = {"one", "two", "three"};
+        for (int i = massive3.length-1; i >= 0 ; i--) {
+            if (i > 0) {
+                System.out.print(massive3[i] + ", ");
+            } else {
+                System.out.print(massive3[i]);
+            }
 
-
-
-
-
-
-
-
+        }
 
 
     }
