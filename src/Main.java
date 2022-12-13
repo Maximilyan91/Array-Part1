83b25524072c1c27367c46a3db346b75a8e841f4
Максим <gmax7911@gmail.com>
2022-12-13 15:13:44 +0300

    Задача 2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+       //Задача 1.
+
+        int[] massive1 = new int[12];
+        massive1[0] = 1;
+        massive1[1] = 2;
+        massive1[2] = 3;
+
+        System.out.print(Arrays.toString(massive1)); // Задача 2.
+        System.out.println();
+
+        double[] massive2 = {1.57, 7.654, 9.986,};
+
+        System.out.print(Arrays.toString(massive2)); // Задача 2.
+        System.out.println();
+
+        String[] massive3 = {"one", "two", "three"};
+
+        System.out.print(Arrays.toString(massive3)); // Задача 2.
+
+
+
 
 
 
